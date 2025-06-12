f4c0da441f36951f10316318b319acb8f6b02725
Shimon <shimonbhuiya02@gmail.com>
2025-06-12 18:01:58 +0600

    DetailsFragment xml

---
+++ app/src/main/java/com/example/foodorderingapp/DetailsFragment.kt
@@ -0,0 +1,24 @@
+package com.example.foodorderingapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.foodorderingapp.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    lateinit var binding:FragmentDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDetailsBinding.inflate(layoutInflater,container,false)
+        return binding.root
+
+    }
+
+
+}
